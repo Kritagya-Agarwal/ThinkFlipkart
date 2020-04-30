2ef7d9d51f4c19a864a27f0c5ba8dca22a6d29af
Kritagya-Agarwal <38519793+Kritagya-Agarwal@users.noreply.github.com>
2020-04-30 14:20:15 +0530

    Update String.cpp
    Added Functionality of toLower() in String.cpp

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -4,6 +4,10 @@ void toUpper(char string[]){
 	printf("Making String Upper\n");
 }
 
+void toLower(char string[]){
+	printf("Making String Lower\n");
+}
+
 void greeting(char something){
 	printf("Hello";
 }
